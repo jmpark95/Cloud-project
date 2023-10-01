a1f614ea0e4a90d3c2c38d1f0cd4999b89df548a
jmpark95 <jmpark95@live.com>
2023-10-01 19:44:20 +1100

    Uploaded to cloud storage using terraform

---
+++ README.md
@@ -0,0 +1,1 @@
+October 1: Created cloud storage bucket and uploaded index.html using terraform
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_storage_bucket" "static-website" {
+  name          = "static-website-minpark95"
+  location      = "AUSTRALIA-SOUTHEAST1"
+  storage_class = "STANDARD"
+  website {
+    main_page_suffix = "index.html"
+  }
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "indexpage" {
+  name         = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+  bucket       = google_storage_bucket.static-website.id
+}
