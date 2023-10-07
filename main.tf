d389ef92f376a0b77513db73e4f40f582ce66bd8
jmpark95 <jmpark95@live.com>
2023-10-07 23:37:59 +1100

    Terraform provisioning load balancer, SSL

--- main.tf
+++ main.tf
@@ -13,28 +13,3 @@ provider "google" {
   region      = var.region
   zone        = var.zone
 }
-
-resource "google_storage_bucket" "static-website" {
-  name          = "static-website-minpark95"
-  location      = "AUSTRALIA-SOUTHEAST1"
-  storage_class = "STANDARD"
-  website {
-    main_page_suffix = "index.html"
-  }
-
-  uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket_object" "indexpage" {
-  name         = "index.html"
-  source       = "index.html"
-  content_type = "text/html"
-  bucket       = google_storage_bucket.static-website.id
-}
-
-resource "google_storage_bucket_iam_member" "member" {
-  provider = google
-  bucket   = google_storage_bucket.static-website.id
-  role     = "roles/storage.objectViewer"
-  member   = "allUsers"
-}
