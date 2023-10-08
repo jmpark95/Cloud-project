a6ca53649248f10ad887153bc84fb833c47da515
jmpark95 <jmpark95@live.com>
2023-10-08 16:44:27 +1100

    changed default execution mode to local

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -14,16 +14,14 @@ resource "google_storage_bucket" "static-website" {
 # Add objects
 resource "google_storage_bucket_object" "indexpage" {
   name         = "index.html"
-  content      = file("${path.module}/../src/index.html")
-  # source       = "../src/index.html"
+  source       = "../src/index.html"
   content_type = "text/html"
   bucket       = google_storage_bucket.static-website.id
 }
 
 resource "google_storage_bucket_object" "script" {
   name = "script.js"
-  content = file("${path.module}/../src/script.js")
-  # source  = "../src/script.js"
+  source  = "../src/script.js"
   bucket  = google_storage_bucket.static-website.id
 }
 
