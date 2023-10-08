6fef41f3e95381c025c789da68609cc46a490ed7
jmpark95 <jmpark95@live.com>
2023-10-08 23:20:59 +1100

    Adding A record to DNS

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -45,3 +45,16 @@ resource "google_compute_url_map" "default" {
   }
 }
 
+resource "google_dns_managed_zone" "my_zone" {
+  name        = "minpark.dev-zone"
+  dns_name    = "minpark.dev"
+}
+
+resource "google_dns_record_set" "a_record" {
+  name         = "minpark.dev"
+  managed_zone = google_dns_managed_zone.my_zone.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_forwarding_rule.default.ip_address]
+}
+
