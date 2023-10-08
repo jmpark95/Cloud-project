b21e1c29f0afcdd445ee905dc84ddafd04b3067d
jmpark95 <jmpark95@live.com>
2023-10-08 11:11:09 +1100

    Organised terraform files

---
+++ terraform/ssl.tf
@@ -0,0 +1,20 @@
+#Create a Google-managed SSL certificate
+resource "google_compute_managed_ssl_certificate" "lb_default" {
+  name     = "terraform-ssl-cert"
+
+  managed {
+    domains = ["minpark.dev"]
+  }
+}
+
+# Update an existing load balancer
+resource "google_compute_target_https_proxy" "lb_default" {
+  name     = "terraform-practice-https-proxy"
+  url_map  = google_compute_url_map.default.id
+  ssl_certificates = [
+    google_compute_managed_ssl_certificate.lb_default.name
+  ]
+  depends_on = [
+    google_compute_managed_ssl_certificate.lb_default
+  ]
+}
