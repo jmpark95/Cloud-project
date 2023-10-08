447b3180879a7acd5583f2f0c977a35cc1fa65f7
jmpark95 <jmpark95@live.com>
2023-10-08 15:33:05 +1100

    3

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -15,7 +15,7 @@ resource "google_storage_bucket" "static-website" {
 resource "google_storage_bucket_object" "indexpage" {
   name         = "index.html"
   # source       = "${path.module}/src/index.html"
-  content = filebase64("${path.module}/src/index.html")
+  content = filebase64("${path.module}/../src/index.html")
   content_type = "text/html"
   bucket       = google_storage_bucket.static-website.id
 }
@@ -23,7 +23,7 @@ resource "google_storage_bucket_object" "indexpage" {
 resource "google_storage_bucket_object" "script" {
   name = "script.js"
   # source       = "${path.module}/src/script.js"
-  content = filebase64("${path.module}/src/script.js")
+  content = filebase64("${path.module}/../src/script.js")
   bucket  = google_storage_bucket.static-website.id
 }
 
