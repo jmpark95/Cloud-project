45cc09681e92fca7ad58c05d01cc7923fe3584eb
jmpark95 <jmpark95@live.com>
2023-10-09 23:29:42 +1100

    4

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -2,6 +2,10 @@
 resource "google_secret_manager_secret" "github_token_secret" {
     project =  var.project
     secret_id = "github"
+
+    replication {
+        auto {}
+    }
 }
 
 resource "google_secret_manager_secret_version" "github_token_secret_version" {
