d42e99cdd61aa9ac85524b190c9882f5e13446cf
jmpark95 <jmpark95@live.com>
2023-10-09 20:49:12 +1100

    google beta

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -2,12 +2,13 @@ resource "google_project_service" "firestore" {
   service = "firestore.googleapis.com"
 }
 
-# resource "google_firestore_database" "database" {
-#   name        = "(default)"
-#   location_id = var.region
-#   type        = "FIRESTORE_NATIVE"
-#   depends_on = [google_project_service.firestore]
-# }
+resource "google_firestore_database" "database" {
+  name        = "(default)"
+  provider    = google-beta
+  location_id = var.region
+  type        = "FIRESTORE_NATIVE"
+  depends_on = [google_project_service.firestore]
+}
 
 resource "google_firestore_document" "viewCountDoc" {
   collection  = "viewCountCollection"
