38727f6a5dd7daa15fec88c9c70df9a960547265
jmpark95 <jmpark95@live.com>
2023-10-09 15:36:30 +1100

    Temporary fix to load viewcount faster

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
    try {
-      await fetch("https://cloud-project-gateway-5g50j8rg.ts.gateway.dev/increment", {
-         method: "POST",
-      });
-
       const response = await fetch("https://cloud-project-gateway-5g50j8rg.ts.gateway.dev/viewcount");
       const data = await response.json();
 
       const viewCountDiv = document.getElementById("viewCount");
       viewCountDiv.textContent = `${data.count} times`;
+
+      await fetch("https://cloud-project-gateway-5g50j8rg.ts.gateway.dev/increment", {
+         method: "POST",
+      });
    } catch (error) {
       console.error("Error:", error);
    }
